ee3c963c487c9ae007e7a351142b26ae89214417
yopnano <yannherledan1@gmail.com>
2022-09-19 18:45:16 +0200

    Ajout du fichier de config dans include

---
+++ include/BeOutilsConfig.h
@@ -0,0 +1,15 @@
+#if !defined(BeOutilsConfig_hg)
+#define BeOutilsConfig_hg
+
+#define ProjectName "BeOutils DEV 0.2 [en cours de test]"
+
+#define UsingLib_Serial
+// #define UsingLib_IR
+// #define UsingLib_433MHz
+// #define UsingLib_Servo
+// #define UsingLib_LcdI2c
+// #define UsingLib_PID
+
+
+
+#endif // BeOutilsConfig_hg
