181b4ae048182139e69c82c1c0f20906931f1360
yopnano <yannherledan1@gmail.com>
2022-09-20 12:24:06 +0200

    Modification du fichier de configuration

--- include/BeOutilsConfig.h
+++ include/BeOutilsConfig.h
@@ -1,15 +1,6 @@
-#if !defined(BeOutilsConfig_hg)
+#ifndef BeOutilsConfig_hg
 #define BeOutilsConfig_hg
 
-#define ProjectName "BeOutils DEV 0.2 [en cours de test]"
-
-#define UsingLib_Serial
-// #define UsingLib_IR
-// #define UsingLib_433MHz
-// #define UsingLib_Servo
-// #define UsingLib_LcdI2c
-// #define UsingLib_PID
-
-
+    #include "../../../../src/Config.h"
 
 #endif // BeOutilsConfig_hg
