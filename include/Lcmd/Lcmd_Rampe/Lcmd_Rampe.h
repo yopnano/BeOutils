7c0e4c38fefc0906cf0670578df34aeb19ede21e
yopnano <yannherledan1@gmail.com>
2022-09-20 19:23:48 +0200

    Correction des erreurs du compilateur

--- include/Lcmd/Lcmd_Rampe/Lcmd_Rampe.h
+++ include/Lcmd/Lcmd_Rampe/Lcmd_Rampe.h
@@ -3,21 +3,18 @@
 
 #include <Arduino.h>
 
-unsigned char 
-
 class Lcmd_Rampe
 {
 private:
     unsigned long m_millisOld;
 
 public:
-    bool disable;
-    unsigned short rampe;
-    
     Lcmd_Rampe(unsigned short rampe_ms = 10);
-
+    
     void main(unsigned char csg, unsigned char &csgActuelle);
-
+    
+    unsigned short rampe;
+    bool disable;
 };
 
 #endif // Lcmd_Rampe_h
