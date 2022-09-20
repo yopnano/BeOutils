143cf9e398a64f25d41fdfd244029bfbe6cd6b6f
yopnano <yannherledan1@gmail.com>
2022-09-20 14:28:14 +0200

    Lcom.h inversion des ifdef config pour 433 et IR

--- include/Lcom/Lcom.h
+++ include/Lcom/Lcom.h
@@ -4,11 +4,11 @@
 #include <BeOutilsConfig.h>
 
     // Appel des Classes de la bibliothèque Lcom
-    #ifdef UsingLib_IR
+    #ifdef UsingLib_433MHz
         #include <Lcom\Lcom_433MHz\Lcom_433MHz.h>
     #endif
 
-    #ifdef UsingLib_433MHz
+    #ifdef UsingLib_IR
         #include <Lcom\Lcom_IR\Lcom_IR.h>
     #endif
     
