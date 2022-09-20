e487205bab16e64065366f928dd30b57db04b070
yopnano <yannherledan1@gmail.com>
2022-09-20 22:06:47 +0200

    Modification LioPwm type boolean -> bool m_pinNumber -> m_pin Création LioPwmTor (pwm on off toggle) Création LioPwmRampe (pwm avec rampe incrémentation / décrémentation)

--- include/Lio/LioPwm/LioPwm.h
+++ include/Lio/LioPwm/LioPwm.h
@@ -2,54 +2,64 @@
 #define LioPwm_h
 
 #include <Arduino.h>
+#include <Lcmd\Lcmd_Rampe\Lcmd_Rampe.h>
 
 class LioPwm
 {
-    protected:
-        boolean m_inverted;
-        uint8_t m_pinNumber;
-        uint8_t m_oldValue;
+protected:
+    bool m_inverted;
+    uint8_t m_pin;
 
-    public:
-        /*! @brief  Etat de la sortie
-        */
-        uint8_t value;
+public:
+    /*! @brief  Sortie PWM.
 
-        
-        /*! @brief  Sortie PWM.
+        @param pin  n° broche sortie PWM
+        @param Inverted pour inverser l'état de la sortie
+    */
+    LioPwm(uint8_t pin, bool inverted = false);
 
-            @param pin  n° broche sortie PWM
-            @param Inverted pour inverser l'état de la sortie
-        */
-        LioPwm(uint8_t pin, boolean inverted = false);
+    /*! @brief  Main PWM.
+        Gestion de la sortie PWM
+    */
+    void main(void);
+    void setup(void);
 
-        /*! @brief  Main PWM.
-
-            Gestion de la sortie PWM
-        */
-        void main(void);
-        void setup(void);
-
-        /*! @brief  Ecriture de la sortie
-
-            @param newVal nouvelle valeur à appliquer
-        */
-        void write(boolean newVal) {value = newVal;}
-
-        
-        /*! @brief  Activer l'actionneur
-        */
-        void turnOn() {value = 255;}
-        
-        /*! @brief  Desactiver l'actionneur
-        */
-        void turnOff(){value = 0;}
-
-        /*! @brief  Inverser l'état de l'actionneur
-            si actionneur en marche -> arrêt
-            si actionneur à l'arrêt -> dernière consigne utilisée
-        */
-        void toggle();
+    /*! @brief  Valeur de la sortie
+     */
+    uint8_t val;
 };
 
+class LioPwmTor : public LioPwm
+{
+private:
+    uint8_t m_oldValue;
+
+public:
+    /*! @brief  Activer l'actionneur
+        La consigne retourne à la valeur précedente
+     */
+    void turnOn();
+
+    /*! @brief  Desactiver l'actionneur
+     */
+    void turnOff();
+
+    /*! @brief  Inverser l'état de l'actionneur
+        si actionneur en marche -> arrêt
+        si actionneur à l'arrêt -> dernière consigne utilisée
+    */
+    void toggle();
+};
+
+class LioPwmRampe : public LioPwm, public Lcmd_Rampe
+{
+public:
+    LioPwmRampe(uint8_t pin, unsigned short rampe_ms = 10, bool inverted = false);
+
+    void main(void);
+
+    /*! @brief  Consigne de la sortie
+     */
+    uint8_t csg;
+};
 #endif
--- src/Lio/LioPwm/LioPwm.cpp
+++ src/Lio/LioPwm/LioPwm.cpp
@@ -1,35 +1,64 @@
 #include <Lio\LioPwm\LioPwm.h>
 
-LioPwm::LioPwm(uint8_t pin, boolean inverted):
-    //Constructeur
-    m_inverted(inverted),
-    m_pinNumber(pin),
-    m_oldValue(0),
-    value(0)
-    {}
+LioPwm::LioPwm(uint8_t pin, bool inverted) : // Constructeur
+                                             m_inverted(inverted),
+                                             m_pin(pin),
+                                             m_oldValue(0),
+                                             val(0)
+{
+}
 
 void LioPwm::setup(void)
 {
-    //Verifie si la pin est PWM
-    if (digitalPinToTimer(m_pinNumber) == NOT_ON_TIMER) {Serial.print (F("LioPwm Warning : pin > ")); Serial.print (m_pinNumber); Serial.println (F(" isn't PWM"));}
-    else pinMode(m_pinNumber, OUTPUT);
+    // Verifie si la pin est PWM
+    if (digitalPinToTimer(m_pin) == NOT_ON_TIMER)
+    {
+        Serial.print(F("LioPwm Warning : pin > "));
+        Serial.print(m_pin);
+        Serial.println(F(" isn't PWM"));
+    }
+    else
+        pinMode(m_pin, OUTPUT);
 }
 
 void LioPwm::main(void)
 {
-    //Borne la consigne entre 0 et 255
-    value = constrain(value, 0, 255);
+    // Borne la consigne entre 0 et 255
+    val = constrain(val, 0, 255);
 
-    //Ecriture de la sortie
-    analogWrite(m_pinNumber, abs((m_inverted ? 255 : 0) - value));
+    // Ecriture de la sortie
+    analogWrite(m_pin, abs((m_inverted ? 255 : 0) - val));
 }
 
-void LioPwm::toggle(void)
+// ======================= Lio Pwm Tor ======================= //
+void LioPwmTor::turnOff(void)
 {
-    if (!m_oldValue &! value) value = 255;
+    if (val)
+        m_oldValue = val;
+    val = 0;
+}
 
-    //Sauvegarde et apllication l'ancienne consigne
-    uint8_t retain = value;
-    value = m_oldValue;
-    m_oldValue = retain;
+void LioPwmTor::turnOn(void)
+{
+    if (!val)
+        val = (m_oldValue) ? m_oldValue : 255;
+}
+
+void LioPwmTor::toggle(void)
+{
+    if (val)
+        turnOff();
+    else
+        turnOn();
+}
+
+// ======================= Lio Pwm Rampe ======================= //
+LioPwmRampe::LioPwmRampe(uint8_t pin, unsigned short rampe_ms, bool inverted) : LioPwm(pin, inverted),
+                                                                                Lcmd_Rampe(rampe_ms),
+                                                                                csg(0){};
+
+void LioPwmRampe::main(void)
+{
+    Lcmd_Rampe::main(csg, val);
+    LioPwm::main();
 }
