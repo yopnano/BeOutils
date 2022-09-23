25cc60e8d701d38968edfa232b81f1c9dc728f7f
yopnano <yannherledan1@gmail.com>
2022-09-23 23:01:27 +0200

    Ajout d'un calcul de temps de cycle

--- include/Lsys/Lsys.h
+++ include/Lsys/Lsys.h
@@ -1,78 +1,56 @@
 #ifndef Lsys_h
 #define Lsys_h
 
-    // #include "../BeOutil.h"
-    #include <Arduino.h>
-    #include <UDT\BitBool\BitBool.h>
-    // Appel des Classes de la bibliothèque Lsys
-
+// #include "../BeOutil.h"
+#include <Arduino.h>
+#include <UDT\BitBool\BitBool.h>
+// Appel des Classes de la bibliothèque Lsys
 
 // ========================================== Clock et Front système ========================================== //
 class Lsys_Fdelay
 {
     // Attributs publics
-    public:
-        /*!
-            @brief  Horloge basculante sur une periode en ms.
+public:
+    Lsys_Fdelay(uint32_t periodMs);
 
-            @see Lsys_Fimpulse.
+    bool clock();
+    bool impulse();
 
-            @param periodMs  Periode de temps de l'impulsion en millisecondes         
-        */
-        Lsys_Fdelay(uint32_t periodMs);
+private:
+    BitBool<8> m_bools;
 
-        /*!
-            @brief  Signal d'horloge basculant sur demi-periode.
-
-            @param periodMs  Periode de temps de basculement en millisecondes         
-        */
-        bool clock();
-        
-        /*!
-            @brief  Sortie de l'impulsion.
-            ne peut être appelée qu'une seule fois vu qu'elle génère un front
-            @return Impulsion booleene           
-        */
-        bool impulse();
-
-    private:
-        BitBool<8> m_bools;
-
-        unsigned long m_period;
-        unsigned long m_millisOld;
+    unsigned long m_period;
+    unsigned long m_millisOld;
 };
-
-
 
 // ========================================== Librairie système ========================================== //
 
-
 class Lsys
 {
-    public:
-        Lsys(void);
+public:
+    enum TimeUnit : byte // Base de temps
+    { 
+        microsecond, 
+        millisecond, 
+        second, 
+        minute,
+        hour 
+    };
 
-        /*!
-            @brief  Methode main.
-            Gestion des bit système. 
-            Doit être applelée dans la fonction loop()          
-        */
-        void main();
+    Lsys(void);
 
-        bool ft10Hz
-            ,ft2Hz
-            ,ft1Hz
-            ,cl1Hz;
-                
-        bool firstscan
-            ,True
-            ,False;
-    private:
-        
-        // instances de clock et pulse
-        Lsys_Fdelay i_d10Hz;
-        Lsys_Fdelay i_d2Hz;
-        Lsys_Fdelay i_d1Hz;
+    void main();
+
+    bool ft10Hz, ft2Hz, ft1Hz, cl1Hz; // Bit de cadence système
+
+    bool firstscan, True, False; // Bit système
+
+    double tpsCycle(Lsys::TimeUnit unite = microsecond);
+
+private:
+    Lsys_Fdelay i_d10Hz, i_d2Hz, i_d1Hz; // Clock et pulse
+
+    unsigned long m_microsOld, m_tpsCycle; // Temps de cycle en microsecondes
 };
 
 static Lsys sys;
--- src/Lsys/Lsys.cpp
+++ src/Lsys/Lsys.cpp
@@ -2,12 +2,14 @@
 
 // ========================================== Clock et Front système ========================================== //
 
-// Constructeur
-Lsys_Fdelay::Lsys_Fdelay(uint32_t periodMs) : m_period(periodMs / 2)
-                                            , m_millisOld(millis())
+/// @brief Horloge basculante sur une periode en ms.
+/// @param periodMs Periode de temps de l'horloge
+Lsys_Fdelay::Lsys_Fdelay(uint32_t periodMs) : m_period(periodMs / 2), m_millisOld(millis())
 {
 }
 
+/// @brief Horloge periodique
+/// @return Signal d'horloge basculant sur demi-periode.
 bool Lsys_Fdelay::clock()
 {
     if (millis() - m_millisOld >= m_period)
@@ -18,41 +20,77 @@ bool Lsys_Fdelay::clock()
     return m_bools[0];
 }
 
+/// @brief Pulsation periodique
+/// @return Impulsion à chaque periode.
 bool Lsys_Fdelay::impulse()
 {
     this->clock();
-    m_bools[1] = m_bools[0] &! m_bools[2]; // Front montant de clock
-    m_bools[2] = m_bools[0]; // Memo clock old
+    m_bools[1] = m_bools[0] & !m_bools[2]; // Front montant de clock
+    m_bools[2] = m_bools[0];               // Memo clock old
 
     return m_bools[1];
 }
 
-
-
-
 // ========================================== Librairie système ========================================== //
 
-    Lsys::Lsys() :
-        i_d10Hz(100),
-        i_d2Hz(500),
-        i_d1Hz(1000)
-    {}
+Lsys::Lsys() : i_d10Hz(100),
+               i_d2Hz(500),
+               i_d1Hz(1000),
+               m_microsOld(micros())
+{
+}
 
-    // Main
-    void Lsys::main()
+/// @brief Methode main.
+/// Gestion des bit système.
+/// Doit être applelée dans la fonction loop()
+void Lsys::main()
+{
+    // Temps de cycle
+    m_tpsCycle = micros() - m_microsOld;
+    m_microsOld = micros();
+
+    // Cadences
+    ft10Hz = i_d10Hz.impulse();
+    ft2Hz = i_d2Hz.impulse();
+    ft1Hz = i_d1Hz.impulse();
+    cl1Hz = i_d1Hz.clock();
+
+    // Bits système
+    firstscan = not True;
+    if (firstscan)
+        True = true;
+    if (False)
+        False = false;
+}
+
+
+/// @brief Temps de cycle.
+/// @param unite type de retour Lsys::TimeUnit
+double Lsys::tpsCycle(Lsys::TimeUnit unite)
+{
+    switch (unite)
     {
+    case microsecond:
+        return m_tpsCycle;
+        break;
 
-        // Cadences
-        ft10Hz = i_d10Hz.impulse();
-        ft2Hz = i_d2Hz.impulse();
-        ft1Hz = i_d1Hz.impulse();
-        cl1Hz = i_d1Hz.clock();
+    case millisecond:
+        return (double) m_tpsCycle / 1000;
+        break;
 
+    case second:
+        return (double) m_tpsCycle / 1000000;
+        break;
 
-        // Bits système
-        firstscan = not True;
-        if (firstscan) True = true;
-        if (False) False = false;
+    case minute:
+        return (double) m_tpsCycle / 60000000;
+        break;
+
+    case hour:
+        return (double) m_tpsCycle / 3600000000;
+        break;
+
+    default:
+        return m_tpsCycle;
     }
-
-  
+}
