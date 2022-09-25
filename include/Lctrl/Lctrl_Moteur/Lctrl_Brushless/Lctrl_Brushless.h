c91079ca027011d58c40852dd8937688c2bd7298
yopnano <yannherledan1@gmail.com>
2022-09-25 17:52:07 +0200

    LctrlBrushless1/2 sens basé sur LctrlMoteurCsg1/2cmd

--- include/Lctrl/Lctrl_Moteur/Lctrl_Brushless/Lctrl_Brushless.h
+++ include/Lctrl/Lctrl_Moteur/Lctrl_Brushless/Lctrl_Brushless.h
@@ -5,75 +5,29 @@
 #include <Servo.h>
 #include <Lctrl\Lctrl_Moteur\LctrlMoteur.h>
 
-class Lctrl_Brushless_1sens : public LctrlMoteurOld
+class Lctrl_Brushless_1sens : public LctrlMoteurCsg1cmd, private Servo
 {
 public:
-    // Enumération  mode de fonctionnement moteur
-    enum ModeBrushless
-    {
-        Arret_forcee,
-        Marche_forcee,
-        Mode_auto = 3,
-        Defaut
-    };
-    
-    Lctrl_Brushless_1sens(unsigned char pin, unsigned char mode, unsigned char rampeAcc = 5, unsigned short dtMin = 1000, unsigned short dtMax = 2000);
+    Lctrl_Brushless_1sens(byte pin, unsigned short rampe_ms = 30, byte min = 0, byte max = 255, unsigned short dtMin = 1000, unsigned short dtMax = 2000);
 
-    /*! @brief  Initialise le moteur automatiquement executé lors du setup */
-    void init(void) {m_initialized = false;}
-    
-    /*! @brief  Retourne true si le moteur est initialisé */
-    bool isReady() const {return m_initialized;}
-
-    void main(void) override;
     void setup(void) override;
+    void out(void) override;
 
 private:
-    bool m2sens() const override {return false;}
-    bool m_initialized = false;
-    unsigned short m_dtMin;
-    unsigned short m_dtMax;
-
-    LMoteurSpeed m_speed;
-    Servo m_servo;
+    byte m_pin;
+    unsigned short m_dtMin, m_dtMax;
 };
 
-
-
-class Lctrl_Brushless_2sens : public LctrlMoteurOld
+class Lctrl_Brushless_2sens : public LctrlMoteurCsg2cmd, private Servo
 {
 public:
-    // Enumération  mode de fonctionnement moteur
-    enum ModeBrushless
-    {
-        Arret_forcee,
-        Marche_AV_forcee,
-        Marche_AR_forcee,
-        Mode_auto,
-        Defaut
-    };
-    
-    Lctrl_Brushless_2sens(unsigned char pin, unsigned char mode, unsigned char rampeAcc = 5, unsigned short dtMin = 1000, unsigned short dtMid = 1500, unsigned short dtMax = 2000);
+    Lctrl_Brushless_2sens(byte pin, unsigned short rampe_ms = 30, byte min = 0, byte max = 255, unsigned short dtMin = 1000, unsigned short dtMid = 1500, unsigned short dtMax = 2000);
 
-    /*! @brief  Initialise le moteur automatiquement executé lors du setup */
-    void init(void) {m_initialized = false;}
-
-    /*! @brief  Retourne true si le moteur est initialisé */
-    bool isReady() const {return m_initialized;}
-
-    void main(void) override;
     void setup(void) override;
-
+    void out(void) override;
 
 private:
-    LMoteurSpeed m_speed;
-    Servo m_servo;
-    
-    bool m2sens() const override {return true;}
-    bool m_initialized = false;
-
-    unsigned short m_dtMin;
-    unsigned short m_dtMid;
-    unsigned short m_dtMax;
+    byte m_pin;
+    unsigned short m_dtMin, m_dtMid, m_dtMax;
 };
 #endif
--- src/Lctrl/Lctrl_Moteur/Lctrl_Brushless/Lctrl_Brushless.cpp
+++ src/Lctrl/Lctrl_Moteur/Lctrl_Brushless/Lctrl_Brushless.cpp
@@ -1,119 +1,74 @@
+#include <Lctrl\Lctrl_Moteur\Lctrl_Brushless\Lctrl_Brushless.h>
 #ifdef UsingLib_Servo
 
-    #include <Lctrl\Lctrl_Moteur\Lctrl_Brushless\Lctrl_Brushless.h>
+/// @brief Controle moteur brushless 1 sens avec ESC
+/// @param pin n° broche du signal
+/// @param rampe_ms Temps Accélération / décélération en ms : 10 = 2.5 sec de 0 à 255
+/// @param min consigne minimum la sortie PWM ne descend pas sous cette valeur
+/// @param max consigne maximum la sortie PWM ne dépasse pas cette valeur
+/// @param dtMin valeur ou le moteur est à l'arrêt
+/// @param dtMax valeur ou le moteur est au maximum
+Lctrl_Brushless_1sens::Lctrl_Brushless_1sens(byte pin, unsigned short rampe_ms, byte min, byte max, unsigned short dtMin, unsigned short dtMax)
+    : LctrlMoteurCsg1cmd(rampe_ms, min, max),
+      Servo(),
+      m_pin(pin),
+      m_dtMin(dtMin),
+      m_dtMax(dtMax)
+{
+}
 
-    /*
-        brushless 1 sens de rotation
-    */
+/*! @brief  Initialiser le moteur */
+void Lctrl_Brushless_1sens::setup(void)
+{
+    this->attach(m_pin, m_dtMin, m_dtMax);
+    this->write(180);
+    delay(2500);
+    this->write(0);
+    delay(2500);
+}
 
-    Lctrl_Brushless_1sens::Lctrl_Brushless_1sens(unsigned char pin, unsigned char mode, unsigned char rampeAcc, unsigned short dtMin, unsigned short dtMax):
-        LctrlMoteurOld(mode, 0, 255, 0, 255, rampeAcc),
-        m_dtMin(dtMin),
-        m_dtMax(dtMax)
-        {
-            m_pin = pin;
-        }
+/// @brief Pilotage de la sortie
+void Lctrl_Brushless_1sens::out(void)
+{
+    this->writeMicroseconds(map(m_val, m_min, m_max, m_dtMin, m_dtMax));
+}
 
-    void Lctrl_Brushless_1sens::setup(void)
-    {
-        if (!m_servo.attached()) m_servo.attach(m_pin, m_dtMin, m_dtMax);
-        init();
-    }
+/// @brief Controle moteur brushless 2 sens avec ESC
+/// @param pin n° broche du signal
+/// @param rampe_ms Temps Accélération / décélération en ms : 10 = 2.5 sec de 0 à 255
+/// @param mini consigne minimum la sortie PWM ne descend pas sous cette valeur
+/// @param maxi consigne maximum la sortie PWM ne dépasse pas cette valeur
+/// @param dtMin valeur ou le moteur est au maximum sens inverse
+/// @param dtMid valeur ou le moteur est à l'arrêt
+/// @param dtMax valeur ou le moteur est au maximum
+Lctrl_Brushless_2sens::Lctrl_Brushless_2sens(byte pin, unsigned short rampe_ms, byte min, byte max, unsigned short dtMin, unsigned short dtMid, unsigned short dtMax)
+    : LctrlMoteurCsg2cmd(rampe_ms, min, max),
+      Servo(),
+      m_pin(pin),
+      m_dtMin(dtMin),
+      m_dtMid(dtMid),
+      m_dtMax(dtMax)
+{
+}
 
-    void Lctrl_Brushless_1sens::main(void)
-    {
-        //Condition d'arrêt
-        if (m_modeFct == Arret_forcee) m_servo.detach();
-        if ((m_modeFct != Arret_forcee) &! m_servo.attached())
-        {
-            m_servo.attach(m_pin);
-            init();
-        }
+/*! @brief  Initialiser le moteur */
+void Lctrl_Brushless_2sens::setup(void)
+{
+    this->attach(m_pin, m_dtMin, m_dtMax);
+    this->write(180);
+    delay(2500);
+    this->write(0);
+    delay(1000);
+    this->write(90);
+    delay(2500);
+}
 
-        //Calcul de la consigne en fonction de l'état de marche
-        m_csgGlobale =  (m_modeFct == Marche_forcee) * m_csgManu +   //Mode manu avant
-                        (m_modeFct == Mode_auto) * m_csgAuto;           //Mode auto
-
-        //Initialisation du moteur
-        if (!m_initialized)
-        {
-            m_servo.write(0);
-            delay(5000);
-            m_initialized = true;
-        }
-
-        //Si changement de consigne et front rampe => acceleration/deceleration
-        m_speed.main(m_csgGlobale, m_csgActuelle, m_rampe);
-
-        //Gestions KmAv KmAr
-        KM();
-
-        //Calcul de la consigne en microsecondes
-        unsigned short csgMicroseconds;
-        if(m_KmAv) csgMicroseconds = map(m_csgActuelle, m_csgMin, m_csgMax, m_dtMin, m_dtMax);
-        if((!m_KmAv) |! m_initialized) csgMicroseconds = m_dtMin;
-
-        //Pilotage de la sortie
-        if (m_initialized) m_servo.writeMicroseconds(csgMicroseconds);
-    }
-
-
-    /*
-        brushless 2 sens de rotation
-    */
-
-    Lctrl_Brushless_2sens::Lctrl_Brushless_2sens(unsigned char pin, unsigned char mode, unsigned char rampeAcc, unsigned short dtMin, unsigned short dtMid, unsigned short dtMax):
-        LctrlMoteurOld(mode, 0, 255, 0, 255, rampeAcc),
-        m_dtMin(dtMin),
-        m_dtMid(dtMid),
-        m_dtMax(dtMax)
-        {
-            m_pin = pin;
-        }
-
-    void Lctrl_Brushless_2sens::setup(void)
-    {
-        if (!m_servo.attached()) m_servo.attach(m_pin, m_dtMin, m_dtMax);
-        init();
-    }
-
-    void Lctrl_Brushless_2sens::main(void)
-    {
-        //Condition d'arrêt
-        if (m_modeFct == Arret_forcee) m_servo.detach();
-        if ((m_modeFct != Arret_forcee) &! m_servo.attached())
-        {
-            m_servo.attach(m_pin);
-            m_servo.writeMicroseconds(m_dtMid);
-            init();
-        }
-
-        //Calcul de la consigne en fonction de l'état de marche
-        m_csgGlobale =  (m_modeFct == Marche_AV_forcee) * m_csgManu + //Mode manu avant
-                        (m_modeFct == Marche_AR_forcee) * m_csgManu + //Mode manu arriere
-                        (m_modeFct == Mode_auto) * (m_cmdAv ^ m_cmdAr) * m_csgAuto; //Mode auto avant XOR arriere
-
-        //Initialisation du moteur
-        if (!m_initialized)
-        {
-            m_servo.write(90); //m_servo.writeMicroseconds(m_dtMid);
-            delay(7000);
-            m_initialized = true;
-        }
-
-        //Si changement de consigne et front rampe => acceleration/deceleration
-        m_speed.main(m_csgGlobale, m_csgActuelle, m_rampe);
-
-        //Gestions KmAv KmAr
-        KM();
-
-        //Calcul de la consigne en microsecondes
-        unsigned short csgMicroseconds;
-        if(m_KmAv) csgMicroseconds = map(m_csgActuelle, m_csgMin, m_csgMax, m_dtMid, m_dtMax);
-        if(m_KmAr) csgMicroseconds = map(m_csgActuelle, m_csgMin, m_csgMax, m_dtMid, m_dtMin);
-        if((!m_KmAv &! m_KmAr) |! m_initialized) csgMicroseconds = m_dtMid;
-
-        //Pilotage de la sortie
-        if (m_initialized) m_servo.writeMicroseconds(csgMicroseconds);
-    }
+/// @brief Pilotage de la sortie
+void Lctrl_Brushless_2sens::out(void)
+{
+    if (m_arr)
+        this->writeMicroseconds(map(m_val, m_min, m_max, m_dtMid, m_dtMin));
+    else
+        this->writeMicroseconds(map(m_val, m_min, m_max, m_dtMid, m_dtMax));
+}
 #endif
